109d09da76f6ba731672b4d9db6bd80325d674a0
Joe Piemeisel <joe@whilejoe.com>
2017-08-22 23:51:28 -0400

    adds Apollo Provider and client to app index. upgrades rewired pkg and styled components plugin now that they work with new react-scripts v

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import 'styles/index.css';
 import App from './App';
 import { render } from 'react-snapshot';
-import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from 'state/store';
-
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
 import registerServiceWorker from './registerServiceWorker';
 
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj6l71pg81npn0191lrufaos5'
+});
+
+const client = new ApolloClient({ networkInterface });
+
 render(
-  <Provider store={store}>
+  <ApolloProvider client={client} store={store}>
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
-  </Provider>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
