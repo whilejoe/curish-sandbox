eec8abfdf1ba3ebf58fbf26b568d226a9124d24d
Joe Piemeisel <joe@whilejoe.com>
2017-09-24 01:37:16 -0400

    removes unneeded redux actions folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-// import { authReducer } from './authReducer';
-// import { inviteReducer } from './inviteReducer';
-// import { storyReducer } from './storyReducer';
 import formReducer from 'state/forms/reducer';
 
 export default combineReducers({
-  // user: authReducer,
-  // invite: inviteReducer,
-  // stories: storyReducer,
   router: routerReducer,
   forms: formReducer
 });
