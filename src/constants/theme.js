e88397618ffc211d180d08043c3fb1daeb64cfc6
Joe Piemeisel <joe@whilejoe.com>
2017-10-01 06:27:42 -0400

    updates primary theme color

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -4,7 +4,7 @@ export const TERTIARY_KEY = 'tertiary';
 export const ERROR_KEY = 'error';
 
 export const THEME = {
-  [PRIMARY_KEY]: 'Tomato',
+  [PRIMARY_KEY]: 'MediumOrchid',
   [SECONDARY_KEY]: 'lightseagreen',
   [TERTIARY_KEY]: 'Tomato',
   [ERROR_KEY]: 'IndianRed'
