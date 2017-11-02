ef0e664fe0980488f806b9a9e7ddca6c05132856
Joe Piemeisel <joe@whilejoe.com>
2017-11-02 03:12:02 -0400

    removes appcache plugin. updates icon size in manifest json

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,8 @@
-// const { compose } = require('react-app-rewired');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
-const rewireAppcachePlugin = require('react-app-rewire-appcache-plugin');
 const rewireLodash = require('react-app-rewire-lodash');
 
 module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env);
-  config = rewireAppcachePlugin(config, env, {
-    cache: ['https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600|Merriweather:400,700'],
-    settings: ['prefer-online'],
-    output: 'manifest.appcache'
-  });
+  config = rewireStyledComponents(config, env, { preprocess: true });
   config = rewireLodash(config, env);
   return config;
 };
