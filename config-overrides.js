9f3de6ca5a4023a1ec75f82e32ee27652c9ccf79
Joe Piemeisel <joe@whilejoe.com>
2017-11-02 03:14:51 -0400

    removes option in rewiredStyledComponents plugin

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ const rewireStyledComponents = require('react-app-rewire-styled-components');
 const rewireLodash = require('react-app-rewire-lodash');
 
 module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env, { preprocess: true });
+  config = rewireStyledComponents(config, env);
   config = rewireLodash(config, env);
   return config;
 };
