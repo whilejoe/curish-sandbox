2a70efd229e7429e2461e4453d06c021e8f2f911
Joe Piemeisel <joe@whilejoe.com>
2017-11-15 20:27:05 -0500

    updates user stories to return some ui while page loads

--- src/routes/UserStories.js
+++ src/routes/UserStories.js
@@ -4,15 +4,18 @@ import NewItemButton from 'components/NewItemButton';
 import StoryCard from 'components/StoryCard';
 
 const UserStories = ({ userData: { loading, user }, location, ...props }) => {
-  if (loading || !user) return <StoryContainer />;
   return (
     <StoryContainer>
       <h1>Your Stories</h1>
       <NewItemButton to={{ pathname: '/write', state: { referrer: location } }} title="new story" />
-      {user.stories && user.stories.length > 0 ? (
-        user.stories.map(story => <StoryCard key={story.id} story={story} referrer={location} />)
+      {!loading ? (
+        user.stories && user.stories.length > 0 ? (
+          user.stories.map(story => <StoryCard key={story.id} story={story} referrer={location} />)
+        ) : (
+          <h2>You haven't written any stories yet :(</h2>
+        )
       ) : (
-        <h2>You haven't written any stories yet :(</h2>
+        'loading...'
       )}
     </StoryContainer>
   );
